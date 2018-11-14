66f5bd88a5db23b0312ca0f5c6dde1cb2b413cc3
Kasper Halme <kaha@kahas-MacBook-Pro.local>
2018-11-14 11:58:38 +0800

    expand candidate contract

--- election/contracts/Election.sol
+++ election/contracts/Election.sol
@@ -1,10 +1,24 @@
 pragma solidity ^0.4.24;
 
 contract Election {
-    // constructor()
-    string public candidate;
+
+    struct Candidate {
+        uint id;
+        string name;
+        uint voteCount;
+    }
+
+    mapping(uint => Candidate) public candidates;
+
+    uint public candidates_count;
 
     function Election () public {
-        candidate = "Candidate 1";
+        add_candidate("Candidate 1");
+        add_candidate("Candidate 2");
+    }
+
+    function add_candidate (string _name) private {
+        candidates_count++;
+        candidates[candidates_count] = Candidate(candidates_count, _name, 0);
     }
 }
